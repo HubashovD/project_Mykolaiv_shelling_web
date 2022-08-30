ad1cf935b43110cd94d16e602a120cbac5d17b6b
HubashovD <34233868+HubashovD@users.noreply.github.com>
2022-08-30 22:35:31 +0300

    Update scrolly.js

--- js/scrolly.js
+++ js/scrolly.js
@@ -7,6 +7,7 @@
                step: ".step",
            })
            .onStepEnter((response) => {
+               var screenWidth = window.screen.width
                console.log(response)
                if (response.index == 0 && response.direction == 'down') {
                    staticMap = document.getElementById("staticMap")
@@ -30,56 +31,114 @@
 
                    map.flyTo([46.70950052897342, 32.41241455078126], 12)
                } else if (response.index == 8) {
-                   const screenWidth = window.screen.width
                    if (screenWidth < 1023) {
-                       map.flyTo([46.748315425279316, 32.30854868888856], 16);
+                       console.log(screenWidth)
+                       map.flyTo([46.748256613681406, 32.310576438903816], 16);
                    } else {
                        map.flyTo([46.748315425279316, 32.30854868888856], 17);
                    }
                } else if (response.index == 9) {
-                   const screenWidth = window.screen.width
                    if (screenWidth < 1023) {
-                       map.flyTo([46.7370444808237, 32.24112610244752], 15);
+                       console.log(screenWidth)
+                       map.flyTo([46.7367430076866, 32.248950004577644], 15);
                    } else {
                        map.flyTo([46.7370444808237, 32.24112610244752], 16);
                    }
 
                } else if (response.index == 10) {
-                   const screenWidth = window.screen.width
                    if (screenWidth < 1023) {
-                       map.flyTo([46.73540474073094, 32.268884181976325], 16);
+                       console.log(screenWidth)
+                       map.flyTo([46.73541944714171, 32.27081537246705], 16);
                    } else {
                        map.flyTo([46.73540474073094, 32.268884181976325], 17);
                    }
                } else if (response.index == 11) {
-                   const screenWidth = window.screen.width
                    if (screenWidth < 1023) {
-                       map.flyTo([46.96941802389087, 32.64308452606202], 15); //
+                       console.log(screenWidth)
+                       map.flyTo([46.96956444840468, 32.65231132507325], 15); //
                    } else {
+                       console.log('for pc')
                        map.flyTo([46.96941802389087, 32.64308452606202], 16); //
                    }
                } else if (response.index == 12) {
-                   map.flyTo([46.76889554304747, 32.89696097373963], 17); //
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.76870447810992, 32.90022253990174], 16); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.76889554304747, 32.89696097373963], 17); //
+                   }
                } else if (response.index == 13) {
-                   map.flyTo([46.93083584821279, 32.45646715164185], 17); //
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.93071129730967, 32.45832324028016], 17); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.93083584821279, 32.45646715164185], 17); //
+                   }
                } else if (response.index == 14) {
                    map.flyTo([46.6673622366812, 32.25723983463976], 12);
                } else if (response.index == 15) {
-                   map.flyTo([46.7234765210204, 32.184276580810554], 17); //
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.72354271447431, 32.18935132026673], 17); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.7234765210204, 32.184276580810554], 17); //
+                   }
                } else if (response.index == 16) {
-                   map.flyTo([46.73018371147653, 32.19735503196717], 17); //
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.730235188879945, 32.20083117485047], 17); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.73018371147653, 32.19735503196717], 17); //
+                   }
                } else if (response.index == 17) {
-                   map.flyTo([46.66392838499259, 32.310372591018684], 16); //
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.66391365909663, 32.31797933578492], 16); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.66392838499259, 32.310372591018684], 16); //
+                   }
                } else if (response.index == 18) {
-                   map.flyTo([46.93471876050394, 32.55411028862], 16); //
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.93149523052133, 32.55686759948731], 16); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.93471876050394, 32.55411028862], 16); //
+                   }
                } else if (response.index == 19) {
-                   map.flyTo([46.74421316246913, 32.812954187393196], 17); //
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.74413229035123, 32.816247940063484], 17); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.74421316246913, 32.812954187393196], 17); //
+                   }
                } else if (response.index == 20) {
                    map.flyTo([46.93075525648501, 32.45782434940339], 17); //
                } else if (response.index == 21) {
-                   map.flyTo([46.683098099041715, 32.494640350341804], 15);
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.68183210646615, 32.50841617584229], 14); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.683098099041715, 32.494640350341804], 15);
+                   }
+
                } else if (response.index == 22) {
-                   map.flyTo([46.80094076583634, 32.41108417510987], 17);
+                   if (screenWidth < 1023) {
+                       console.log(screenWidth)
+                       map.flyTo([46.801080306695276, 32.41592288017274], 16); //
+                   } else {
+                       console.log('for pc')
+                       map.flyTo([46.80094076583634, 32.41108417510987], 17);
+                   }
+
+
                } else if (response.index == 23) {
                    elem = document.getElementsByClassName("shelling-points")
                    for (var i = 0; i < elem.length; i++) {
